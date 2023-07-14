f9503b28ea246ba0c77679f586b45a49a8997459
MartineThompson <112218252+MartineThompson@users.noreply.github.com>
2023-07-13 20:54:24 -0400

    Add files via upload

---
+++ projectJS.js
@@ -0,0 +1,7 @@
+function generateMealPLan(){
+   nameInput= document.getElementById("name").value;
+   emailInput= document.getElementById("email").value;
+   goalInpit= document.getElementById("goal").value;
+
+
+}
